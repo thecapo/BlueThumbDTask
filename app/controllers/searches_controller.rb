83a32ed7cfccf07402257deab0a9323647650d64
thecapo <the7thcapo@gmail.com>
2018-04-05 15:58:22 +1000

    displayed photo with search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,10 @@
 class SearchesController < ApplicationController
   
   def index
-    @searches = flickr.photos.getRecent
+    @searches = flickr.photos.search(text:"car")
   end
 end
+
+# https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
+
+# https://farm1.staticflickr.com/890/41203363202_c4a0bfb61f.jpg
