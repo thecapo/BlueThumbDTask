c7b1c182e4a27368faa73a398d5c6ea749288168
thecapo <the7thcapo@gmail.com>
2018-04-06 21:29:42 +1000

    first attempt for rspec features

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
 # BlueThumb Development Task
 
+#### Gems Added
 * added flickraw gem
 * added capybara
 * added Rspec
+
+#### Description:
+
+* The Flickr API has a method which allows you to search photos. Create a Ruby on Rails web application that allows users to search for images. 
+
+* The requirements below are purposely light. The devil’s in the detail and it’s the little things that count.
+
+###### Requirements:
+
+* Build app the lets the user performs a search
+
+* Show results when the user clicks on a search result
+
+* Test with RSpec
+
+* Commit to Github
+
+* Deploy to Heroku
+
+* Send GitHub and Heroku links for review.
+
--- spec/features/searches_spec.rb
+++ spec/features/searches_spec.rb
@@ -1,5 +1,63 @@
 require 'rails_helper'
 
 RSpec.feature "Searches", type: :feature do
- 
+  context "user search box" do
+    scenario "nothing happens" do
+      visit root_path
+        within('form') do
+        end
+        click_button 'Search'
+        expect(page).to have_no_xpath("//img")
+    end
+
+    scenario "should display images" do
+      visit root_path
+        within('form') do
+          fill_in 'Search Photos', with: 'cars'
+        end
+        click_button 'Search'
+        expect(page).to have_xpath("//img")
+    end
+
+    scenario "should check presence of input" do
+      visit root_path
+        within('form') do
+        end
+        expect(page).to have_selector("//input")
+    end
+
+    scenario "should check search button" do
+      visit root_path
+        within('form') do
+        end
+        expect(page).to have_button("Search")
+    end
+
+    scenario "should find input field name Search Photos " do
+      visit root_path
+        within('form') do
+        end
+        expect(page).to have_field("Search Photos")
+    end
+  end
+
+  context "shown images" do
+    scenario "should have link on each image" do
+      visit root_path
+        within('form') do
+          fill_in 'Search Photos', with: 'cars'
+        end
+        click_button 'Search'
+        expect(page).to have_link
+    end
+
+    scenario "should show only clicked image" do
+      visit root_path
+        within('form') do
+          fill_in 'Search Photos', with: 'cars'
+        end
+        click_button 'Search'
+        (page).find([:href]).click
+    end
+  end
 end
